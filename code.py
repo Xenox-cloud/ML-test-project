717a534ead48438c85344acf5aca305848980705
Xenox-cloud <89069025+Xenox-cloud@users.noreply.github.com>
2025-09-09 22:11:08 +0600

    numpy plot (#1)

--- code.py
+++ code.py
@@ -1,10 +1,18 @@
 import numpy as np
 import pandas as pd
-
+import matplotlib.pyplot as plt
 
 
 
 a={'col1':213,'col2':123,'col3':456}
 data = pd.DataFrame([a])
-data.to_excel("data.xlsx")
+data.to_csv("data.csv")
 print(data)
+pkq=data.loc[0,"col1"]
+print(pkq)
+
+
+x=np.linspace(0,4*np.pi,100)
+y=np.cos(x)
+plt.plot(x,y)
+plt.show()
