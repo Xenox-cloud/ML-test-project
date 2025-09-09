3664a03fa89a3434720e76be7960e8688671ec51
Xenox-cloud <musfiq56.ma@gmail.com>
2025-09-09 20:53:23 +0600

    numpy practice creating dataframe

--- code.py
+++ code.py
@@ -1,2 +1,10 @@
-import tensorflow as tf
-from tensorflow.keras import layers, models
+import numpy as np
+import pandas as pd
+
+
+
+
+a={'col1':213,'col2':123,'col3':456}
+data = pd.DataFrame([a])
+data.to_excel("data.xlsx")
+print(data)
